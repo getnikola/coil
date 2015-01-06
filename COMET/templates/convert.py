18950be96517849e9defdfc7a21f665af4df36c9
Chris Warrick <kwpolska@gmail.com>
2015-01-06 12:29:27 +0100

    jinjify script
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

---
+++ COMET/templates/convert.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import io
+import os
+import sys
+sys.path.append('/home/kwpolska/git/nikola/scripts')
+import jinjify
+
+files = os.listdir('mako/')
+for i in files:
+    with io.open('mako/' + i, 'r', encoding='utf-8') as fh:
+        f = fh.readlines()
+    o = jinjify.mako2jinja(f)
+    with io.open('jinja/' + i, 'w', encoding='utf-8') as fh:
+        fh.writelines(o)
